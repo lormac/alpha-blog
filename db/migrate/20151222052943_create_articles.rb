e47534777285a941263569d44be7c9a67caca8dd
rails_projects <lorilynnmac@gmai.com>
2015-12-22 08:56:17 +0000

    ready to set up show action

---
+++ db/migrate/20151222052943_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :description
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps null: false
+    end
+  end
+end
